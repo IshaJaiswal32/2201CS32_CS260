ce44995107f5409db18dc26509fa407a553883da
IshaJaiswal32 <129324426+IshaJaiswal32@users.noreply.github.com>
2024-04-13 12:28:28 +0530

    Update tut07_answers.sql

--- DBMS_LAB_CS260_2024/tut07/tut07_answers.sql
+++ DBMS_LAB_CS260_2024/tut07/tut07_answers.sql
@@ -29,6 +29,24 @@ CREATE TABLE projects (
     end_date DATE
 );
 
+INSERT INTO departments (department_id, department_name, location, manager_id) VALUES
+	('1', 'Engineering', 'New Delhi', '3'),
+	('2', 'Sales', 'Mumbai', '5'),
+	('3', 'Finance', 'Kolkata', '4');
+
+INSERT INTO employees (emp_id, first_name, last_name, salary, department_id) VALUES
+	('1', 'Rahul', 'Kumar', '60000', '1'),
+	('2', 'Neha', 'Sharma', '55000', '2'),
+	('3', 'Krishna', 'Singh', '62000', '1'),
+	('4', 'Pooja', 'Verma', '58000', '3'),
+	('5', 'Rohan', 'Gupta', '59000', '2');
+
+INSERT INTO projects (project_id, project_name, budget, start_date, end_date) VALUES
+	('101', 'ProjectA', '100000', '2023-01-01', '2023-06-30'),
+	('102', 'ProjectB', '80000', '2023-02-15', '2023-08-15'),
+	('103', 'ProjectC', '120000', '2023-03-20', '2023-09-30');
+
+
 --Procedure 1
 CREATE PROCEDURE CalculateAverageSalary(departmentID INT)
 BEGIN
